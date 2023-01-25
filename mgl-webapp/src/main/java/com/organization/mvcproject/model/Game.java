967a4797eba4a561ab9943d2e425b09f866d32f2
john.garbero <john.garbero@AFSWW6AYH1GR5E.us.accenturefederal.com>
2023-01-25 15:05:54 -0600

    accomplished most todos in task one

---
+++ mgl-webapp/src/main/java/com/organization/mvcproject/model/Game.java
@@ -0,0 +1,36 @@
+package com.organization.mvcproject.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Game {
+	
+	private Long id;
+	private String name;
+	private String genre;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long game_id) {
+		this.id = game_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String game_name) {
+		this.name = game_name;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String game_genre) {
+		this.genre = game_genre;
+	}
+
+}
